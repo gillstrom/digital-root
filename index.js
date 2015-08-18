31852f5858034eeab42267152eaded55ff80453a
Andreas Gillström <andreasgillstrom@gmail.com>
2015-08-18 18:53:55 +0200

    Use `digit-sum` module

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
 var numberIsInteger = require('number-is-integer');
 var isPositive = require('is-positive');
-var mathSum = require('math-sum');
-var numArr = require('num-arr');
+var digitSum = require('digit-sum');
 
 module.exports = function (val) {
 	if (!numberIsInteger(val) || !isPositive(val)) {
@@ -10,7 +9,7 @@ module.exports = function (val) {
 	}
 
 	while (val.toString().length !== 1) {
-		val = mathSum(numArr(val));
+		val = digitSum(val);
 	}
 
 	return val;
