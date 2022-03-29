b04597ce449252923be8d9b7d762686ed40962e6
Saurav KumaR <chunnu.bihat@gmail.com>
2022-03-29 20:47:06 +0530

    removed redundancy by eliminating loops

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 'use strict';
 var numberIsInteger = require('number-is-integer');
 var numberLength = require('number-length');
-var isPositive = require('is-positive');
-var digitSum = require('digit-sum');
 
 module.exports = function (val) {
 	if (!numberIsInteger(val) || !isPositive(val)) {
 		throw new TypeError('Expected a positive number');
 	}
 
-	while (numberLength(val) !== 1) {
-		val = digitSum(val);
-	}
+	val = val % 9;
 
 	return val;
 };
