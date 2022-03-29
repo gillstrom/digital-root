5144158f698e05f8c4a8abc6a0bae32aebc961fc
SKR301 <chunnu.bihat@gmail.com>
2022-03-29 21:30:46 +0530

    added test title

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import m from './index.js';
 // const m = require('./index');
 
-test(t => {
+test("tests", t => {
 	t.is(m(65536), 7);
 	t.is(m(1853), 8);
 	t.is(m(2035), 1);
